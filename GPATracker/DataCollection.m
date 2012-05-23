423ad569eb645325e5cba65adc382415535bf136
Your Name <terry@hannontechnology.com>
2012-05-23 10:06:44 -0700

    Corrected Company Name on some files that were missing it.

--- GPATracker/DataCollection.m
+++ GPATracker/DataCollection.m
@@ -3,7 +3,7 @@
 //  GPATracker
 //
 //  Created by terryah on 12-05-22.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Hannon Technology. All rights reserved.
 //
 
 #import "DataCollection.h"
