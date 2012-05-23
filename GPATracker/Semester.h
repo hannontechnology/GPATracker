f3c0ebb339f46e8c4f0e0aa22e97e893f8a5aa18
Your Name <terry@hannontechnology.com>
2012-05-22 18:51:44 -0700

    Semester Class Started

---
+++ GPATracker/Semester.h
@@ -0,0 +1,17 @@
+//
+//  Semester.h
+//  GPATracker
+//
+//  Created by terryah on 12-05-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Semester : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * semesterName;
+
+@end
---
+++ GPATracker/Semester.m
@@ -0,0 +1,16 @@
+//
+//  Semester.m
+//  GPATracker
+//
+//  Created by terryah on 12-05-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Semester.h"
+
+
+@implementation Semester
+
+@dynamic semesterName;
+
+@end
