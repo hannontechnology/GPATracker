27edd66158772d93c9fbf21bc04ebe8dd477d513
Aiste Guden <aisteguden@Terrys-Mac-mini.local>
2012-07-14 15:19:05 -0700

    Commit: DataCollection for retrieve semester list. Storyboard fix, SemesterTableView implementations.

---
+++ SemesterEditTableView.h
@@ -0,0 +1,13 @@
+//
+//  SemesterEditTableView.h
+//  GPATracker
+//
+//  Created by Aiste Guden on 12-07-14.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SemesterEditTableView : UITableViewController
+
+@end
