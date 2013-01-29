446810b80801feb0cf897a21bfad26318f6fbc77
Your Name <terry@hannontechnology.com>
2013-01-29 07:27:49 -0800

    Modified Grading Scheme Cell

--- GPATracker/GradingSchemeCell1.h
+++ GPATracker/GradingSchemeCell1.h
@@ -11,6 +11,9 @@
 @interface GradingSchemeCell1 : UITableViewCell
 @property (weak, nonatomic) IBOutlet UILabel *cellLabel1;
 @property (weak, nonatomic) IBOutlet UITextField *cellField1;
+@property (weak, nonatomic) IBOutlet UITextField *minGrade;
+@property (weak, nonatomic) IBOutlet UIButton *btnInGPA;
+@property (weak, nonatomic) IBOutlet UIButton *btnPassFail;
 @property (strong, nonatomic) NSIndexPath *indexPath;
 
 @end
