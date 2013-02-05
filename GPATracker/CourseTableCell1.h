6a5e518d9f12b8ff75eaa5fe78f321667faf2f88
Your Name <terry@hannontechnology.com>
2013-02-05 10:13:11 -0800

    CourseTableCell1

---
+++ GPATracker/CourseTableCell1.h
@@ -0,0 +1,18 @@
+//
+//  SemesterListTableCell1.h
+//  GPATracker
+//
+//  Created by Terry Hannon on 12-08-06.
+//  Copyright (c) 2012 Hannon Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CourseTableCell1 : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *cellLabel1;
+@property (weak, nonatomic) IBOutlet UILabel *cellLabel2;
+@property (weak, nonatomic) IBOutlet UILabel *cellLabel3;
+@property (weak, nonatomic) IBOutlet UILabel *cellLabelGPA;
+
+@end
---
+++ GPATracker/CourseTableCell1.m
@@ -0,0 +1,33 @@
+//
+//  SemesterListTableCell1.m
+//  GPATracker
+//
+//  Created by Terry Hannon on 12-08-06.
+//  Copyright (c) 2012 Hannon Technology Inc. All rights reserved.
+//
+
+#import "CourseTableCell1.h"
+
+@implementation CourseTableCell1
+@synthesize cellLabel1;
+@synthesize cellLabel2;
+@synthesize cellLabel3;
+@synthesize cellLabelGPA;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
