6fd54eb821e825721aa39098d771d1b30b97dac9
David Stevens <davids@hannontechnology.com>
2013-03-13 07:40:39 +0900

    Created new class to link to Syllabus Breakdown view on storyboard.

---
+++ GPATracker/SyllabusEditTableView.h
@@ -0,0 +1,13 @@
+//
+//  SyllabusEditTableView.h
+//  GPATracker
+//
+//  Created by David Stevens on 13-03-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyllabusEditTableView : UITableViewController
+
+@end
